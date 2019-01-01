15d3bb193d9107b6883838764cabcfdc8797a907
qiao512 <46294234+qiao512@users.noreply.github.com>
2019-01-01 17:32:19 +0800

    Add files via upload

---
+++ Desay.DataReader/DataReaderHelper.cs
@@ -0,0 +1,63 @@
+﻿using Desay.DataManager;
+using EZ.Utils;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desay.DataReader
+{
+    public static class DataReaderHelper
+    {
+        public static string GetDataUniqueId(string path, string machineName, string dataName, bool isForBack=false)
+        {
+            DirectoryInfo info = new DirectoryInfo(path);
+            string folder = info.Parent.Name;
+            string id = string.Empty;
+            if (isForBack==false)
+            {
+                id = string.Format("{0}{1}{2}TEMP", folder, machineName, dataName);
+            }
+            else
+            {
+                id = string.Format("{0}{1}{2}{3}", folder, machineName, dataName, info.CreationTime.Ticks);
+            }
+            return id.ToMD5();
+        }
+
+        public static string GetDataName(string path)
+        {
+            return Path.GetFileNameWithoutExtension(path);
+        }
+
+        public static FLEXSA_DATA_TYPE JudgeFlexaDataFileType(string fileName)
+        {
+            string fname = Path.GetFileName(fileName).ToUpper();
+            string[] flexsaDataTypes = Enum.GetNames(typeof(FLEXSA_DATA_TYPE));
+            foreach (string dtype in flexsaDataTypes)
+            {
+                if (fname.StartsWith(dtype))
+                {
+                    return (FLEXSA_DATA_TYPE)Enum.Parse(typeof(FLEXSA_DATA_TYPE), dtype);
+                }
+            }
+
+            return FLEXSA_DATA_TYPE.NOT_SUPPORTED;
+
+        }
+
+        public static bool IsValidFlexaDataFile(string fileName)
+        {
+            string fname = Path.GetFileNameWithoutExtension(fileName).ToUpper();
+            string[] flexsaDataTypes = Enum.GetNames(typeof(FLEXSA_DATA_TYPE));
+            foreach (string dtype in flexsaDataTypes)
+            {
+                if (fname.StartsWith(fname))
+                    return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ Desay.DataReader/ExtensionCenter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desay.DataReader
+{
+    public static class ExtensionCenter
+    {
+        public static DateTime TimeStampToDateTime(this string timestamp)
+        {
+            //2018072700205170
+
+            int year = int.Parse(timestamp.Substring(0, 4));
+            int month = int.Parse(timestamp.Substring(4, 2));
+            int day = int.Parse(timestamp.Substring(6, 2));
+            int hour = int.Parse(timestamp.Substring(8, 2));
+            int minute = int.Parse(timestamp.Substring(10, 2));
+            int second = int.Parse(timestamp.Substring(12, 2));
+            int msecond = int.Parse(timestamp.Substring(14));
+            DateTime t = new DateTime(year, month, day, hour, minute, second, msecond);
+            return t;
+        }
+    }
+}
