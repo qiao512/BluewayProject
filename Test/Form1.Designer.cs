19ae2a0e334872c1384cd0d90ec018644c663abe
qiao512 <46294234+qiao512@users.noreply.github.com>
2019-01-01 17:28:39 +0800

    Add files via upload

---
+++ Test/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
+            this.btTEST = new System.Windows.Forms.Button();
+            this.ckIsForBack = new System.Windows.Forms.CheckBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btSelectAFile = new System.Windows.Forms.Button();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.btTestBlueWayITF = new System.Windows.Forms.Button();
+            this.buttonTestAssembly = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // fileSystemWatcher1
+            // 
+            this.fileSystemWatcher1.EnableRaisingEvents = true;
+            this.fileSystemWatcher1.SynchronizingObject = this;
+            // 
+            // btTEST
+            // 
+            this.btTEST.Location = new System.Drawing.Point(345, 93);
+            this.btTEST.Name = "btTEST";
+            this.btTEST.Size = new System.Drawing.Size(75, 23);
+            this.btTEST.TabIndex = 0;
+            this.btTEST.Text = "测试";
+            this.btTEST.UseVisualStyleBackColor = true;
+            this.btTEST.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // ckIsForBack
+            // 
+            this.ckIsForBack.AutoSize = true;
+            this.ckIsForBack.Checked = true;
+            this.ckIsForBack.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.ckIsForBack.Location = new System.Drawing.Point(213, 100);
+            this.ckIsForBack.Name = "ckIsForBack";
+            this.ckIsForBack.Size = new System.Drawing.Size(126, 16);
+            this.ckIsForBack.TabIndex = 1;
+            this.ckIsForBack.Text = "当前文件为BAK文件";
+            this.ckIsForBack.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(118, 69);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(273, 21);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(59, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "文件路径";
+            // 
+            // btSelectAFile
+            // 
+            this.btSelectAFile.Location = new System.Drawing.Point(397, 67);
+            this.btSelectAFile.Name = "btSelectAFile";
+            this.btSelectAFile.Size = new System.Drawing.Size(75, 23);
+            this.btSelectAFile.TabIndex = 4;
+            this.btSelectAFile.Text = "挑选文件";
+            this.btSelectAFile.UseVisualStyleBackColor = true;
+            this.btSelectAFile.Click += new System.EventHandler(this.btSelectAFile_Click);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog1";
+            this.openFileDialog.Filter = "FujiLogFile|*.dbl";
+            // 
+            // btTestBlueWayITF
+            // 
+            this.btTestBlueWayITF.Location = new System.Drawing.Point(118, 138);
+            this.btTestBlueWayITF.Name = "btTestBlueWayITF";
+            this.btTestBlueWayITF.Size = new System.Drawing.Size(273, 55);
+            this.btTestBlueWayITF.TabIndex = 5;
+            this.btTestBlueWayITF.Text = "测试蓝微接口";
+            this.btTestBlueWayITF.UseVisualStyleBackColor = true;
+            this.btTestBlueWayITF.Click += new System.EventHandler(this.btTestBlueWayITF_Click);
+            // 
+            // buttonTestAssembly
+            // 
+            this.buttonTestAssembly.Location = new System.Drawing.Point(118, 199);
+            this.buttonTestAssembly.Name = "buttonTestAssembly";
+            this.buttonTestAssembly.Size = new System.Drawing.Size(273, 55);
+            this.buttonTestAssembly.TabIndex = 6;
+            this.buttonTestAssembly.Text = "测试蓝微组装";
+            this.buttonTestAssembly.UseVisualStyleBackColor = true;
+            this.buttonTestAssembly.Click += new System.EventHandler(this.buttonTestAssembly_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(118, 260);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(273, 55);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Test DAT";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(665, 367);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.buttonTestAssembly);
+            this.Controls.Add(this.btTestBlueWayITF);
+            this.Controls.Add(this.btSelectAFile);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.ckIsForBack);
+            this.Controls.Add(this.btTEST);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.FileSystemWatcher fileSystemWatcher1;
+        private System.Windows.Forms.Button btTEST;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.CheckBox ckIsForBack;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btSelectAFile;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.Button btTestBlueWayITF;
+        private System.Windows.Forms.Button buttonTestAssembly;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Test/Form1.cs
@@ -0,0 +1,207 @@
+﻿
+using Desay.DataManager;
+using Desay.DataReader;
+using Desay.WinService;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            DateTime now = DateTime.Now;
+            now = AssemblyMachineDataReader.RoundTime(now, 10);
+            MessageBox.Show(now.ToString("yyyy-MM-dd HH:mm:ss"));
+            return;
+
+            Desay.WinService.MachineMonitor monitor = new Desay.WinService.MachineMonitor();
+            monitor.CreateMonitors();
+            /*
+            string path22 = @"D:\项目\蓝微\FUJI FILES\NXT01\bak\2";
+            string[] dddddd = Directory.GetDirectories(path22,"*",SearchOption.TopDirectoryOnly);
+            StringBuilder sbb = new StringBuilder();
+            foreach (string dd in dddddd)
+                sbb.AppendLine(dd);
+            MessageBox.Show(sbb.ToString());
+            return;
+
+            string path = @"D:\项目\蓝微\FUJI FILES\NXT01\bak\2\8095\BPRODUCT_2010.dbl";
+            path = this.textBox1.Text;
+
+            string fileName = System.IO.Path.GetFileNameWithoutExtension(path);
+            fileName = fileName.ToUpper();
+
+            string machineName = "NXT001";
+
+            bool isForBack = this.ckIsForBack.Checked;
+
+            if (fileName.StartsWith(FLEXSA_DATA_TYPE.BPRODUCT.ToString()))
+            {
+                BPRODUCTDataReader reader = new BPRODUCTDataReader(machineName, path);
+                reader.IsForBak = isForBack;
+                reader.LoadData();
+                reader.SaveData();
+            }
+            else if (fileName.StartsWith(FLEXSA_DATA_TYPE.CYC_TIME.ToString()))
+            {
+                CYC_TIMEDataReader reader = new CYC_TIMEDataReader(machineName, path);
+                reader.IsForBak = isForBack;
+                reader.LoadData();
+                reader.SaveData();
+            }
+            else if (fileName.StartsWith(FLEXSA_DATA_TYPE.FD_COUNT.ToString()))
+            {
+                FD_COUNTDataReader reader = new FD_COUNTDataReader(machineName, path);
+                reader.IsForBak = isForBack;
+                reader.LoadData();
+                reader.SaveData();
+            }
+            else if (fileName.StartsWith(FLEXSA_DATA_TYPE.FD_ERROR.ToString()))
+            {
+                FD_ERRORDataReader reader = new FD_ERRORDataReader(machineName, path);
+                reader.IsForBak = isForBack;
+                reader.LoadData();
+                reader.SaveData();
+            }
+            else if (fileName.StartsWith(FLEXSA_DATA_TYPE.FDRARR_L.ToString()))
+            {
+                if (isForBack)
+                {
+                    FDRARR_LDataReader reader = new FDRARR_LDataReader(machineName, path);
+                    reader.IsForBak = isForBack;
+                    reader.LoadData();
+                    reader.SaveData();
+                }
+                else
+                {
+                    FDRARR_LRunningDataReader reader = new FDRARR_LRunningDataReader(machineName, path);
+                    reader.IsForBak = isForBack;
+                    reader.LoadData();
+                    reader.SaveData();
+                }
+            }
+            else if (fileName.StartsWith(FLEXSA_DATA_TYPE.MAC_UTIL.ToString()))
+            {
+                MAC_UTILDataReader reader = new MAC_UTILDataReader(machineName, path);
+                reader.IsForBak = isForBack;
+                reader.LoadData();
+                reader.SaveData();
+            }
+            else if (fileName.StartsWith(FLEXSA_DATA_TYPE.MACERROR.ToString()))
+            {
+                MACERRORDataReader reader = new MACERRORDataReader(machineName, path);
+                reader.IsForBak = isForBack;
+                reader.LoadData();
+                reader.SaveData();
+            }
+            else if (fileName.StartsWith(FLEXSA_DATA_TYPE.NZLERROR.ToString()))
+            {
+                NZLERRORDataReader reader = new NZLERRORDataReader(machineName, path);
+                reader.IsForBak = isForBack;
+                reader.LoadData();
+                reader.SaveData();
+
+            }
+            else if (fileName.StartsWith(FLEXSA_DATA_TYPE.NZLPOS_L.ToString()))
+            {
+                NZLPOS_LDataReader reader = new NZLPOS_LDataReader(machineName, path);
+                reader.IsForBak = isForBack;
+                reader.LoadData();
+                reader.SaveData();
+            }
+            else if (fileName.StartsWith(FLEXSA_DATA_TYPE.NZLUSAGE.ToString()))
+            {
+                NZLUSAGEDataReader reader = new NZLUSAGEDataReader(machineName, path);
+                reader.IsForBak = isForBack;
+                reader.LoadData();
+                reader.SaveData();
+            }
+            else if (fileName.StartsWith(FLEXSA_DATA_TYPE.PD_ERRINF.ToString()))
+            {
+                PD_ERRINFDataReader reader = new PD_ERRINFDataReader(machineName, path);
+                reader.IsForBak = isForBack;
+                reader.LoadData();
+                reader.SaveData();
+            }
+            else if (fileName.StartsWith(FLEXSA_DATA_TYPE.PD_ERROR.ToString()))
+            {
+                PD_ERRORDataReader reader = new PD_ERRORDataReader(machineName, path);
+                reader.IsForBak = isForBack;
+                reader.LoadData();
+                reader.SaveData(); 
+            }
+            else if (fileName.StartsWith(FLEXSA_DATA_TYPE.PPRODUCT.ToString()))
+            {
+                PPRODUCTDataReader reader = new PPRODUCTDataReader(machineName, path);
+                reader.IsForBak = isForBack;
+                reader.LoadData();
+                reader.SaveData();
+            }
+            else if (fileName.StartsWith(FLEXSA_DATA_TYPE.RECIPE_L.ToString()))
+            {
+                RECIPEDataReader reader = new RECIPEDataReader(machineName, path);
+                reader.IsForBak = isForBack;
+                reader.LoadData();
+                reader.SaveData();
+            }
+            */
+          
+        }
+
+        private void btSelectAFile_Click(object sender, EventArgs e)
+        {
+            if (this.openFileDialog.ShowDialog()==DialogResult.OK)
+            {
+                this.textBox1.Text = this.openFileDialog.FileName;
+            }
+        }
+
+        private void btTestBlueWayITF_Click(object sender, EventArgs e)
+        {
+            //string ss = Desay.DataReader.ServerLoader.ItfDataService.GetAdsMesFeedInfo("SMT2018");
+            //object oo = JsonConvert.DeserializeObject<ITF_FEEDER_JSON>(ss);
+            //string dd = "";
+
+            // Desay.WinService.InterfaceMonitor monitor = new Desay.WinService.InterfaceMonitor();
+            // monitor.CreateMonitors();
+
+            AOISPIInterface itf = new AOISPIInterface();
+            string line_no = "SMT-26";
+            itf.GetData(line_no);
+        }
+
+        private void buttonTestAssembly_Click(object sender, EventArgs e)
+        {
+            AssemblyMonitor mm = new AssemblyMonitor();
+            mm.CreateMonitors();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Filter = "txt|*.txt";
+            if(dlg.ShowDialog()==DialogResult.OK)
+            {
+                string ff = dlg.FileName;
+                MessageBox.Show(ff);
+            }
+        }
+    }
+}
